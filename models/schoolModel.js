53bdf2f27240371d953ce388f3469cf20a3f4afc
Navaneeth Gade <navaneethgade07@gmail.com>
2024-08-25 19:57:38 +0530

    updating Model

--- models/schoolModel.js
+++ models/schoolModel.js
@@ -4,7 +4,13 @@ const schools = {
 
     create: (school,cb)=>{
         const sql = 'INSERT INTO schools (Name , address , latitude , longitude) VALUES(?,?,?,?)';
-        db.query(sql,[school.Name , school.address , school.latitude , school.longitude] , cb);
+        db.query(sql,[school.Name , school.address , school.latitude , school.longitude] , (err,result)=>{
+            if(err){
+                console.error('Error adding school:', err);
+                return cb(err, null); // Early return on error
+            }
+            cb(null, result);
+        });
     },
 
     find: (cb)=>{
@@ -12,7 +18,7 @@ const schools = {
         db.query(sql , (err,results)=>{
             if(err){
                 console.log('Error retrieving schools:' , err);
-                cb(err,null);
+                return cb(err,null);
             }
             cb(null,results);
         });
